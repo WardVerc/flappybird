d039029945f100efa92be91eaf093f410d438633
Ward Vercruyssen <ward.vercruyssen@hotmail.be>
2024-05-09 18:05:35 +0200

    flappy: basic gameplay with score

---
+++ scripts/bottom_pipe.gd
@@ -0,0 +1,16 @@
+extends AnimatableBody2D
+
+@onready var player = $"../../Player"
+@onready var game_manager = %GameManager
+
+var has_passed_pipe = false
+
+func _process(delta):
+	check_pass()
+
+func check_pass():
+	if player.position.x > position.x and not has_passed_pipe:
+		game_manager.add_point()
+		has_passed_pipe = true
+	elif player.position.x <= position.x: 
+		has_passed_pipe = false
